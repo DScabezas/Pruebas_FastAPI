b580822b64a38f124ff9daa4a45c21b169248c80
Dario <dariosce99@gmail.com>
2026-01-27 10:48:19 -0500

    CRUD de Usuarios

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from sqlmodel import SQLModel
+
+app = FastAPI()
+
+class Usuario(SQLModel):
+    id: int
+    nombre: str
+    password: str
+
+usuarios = []
+
+@app.get("/")
+def root():
+    return {"message": "Hello world"}
+
+@app.get("/user")
+def obtener_usuarios():
+    return usuarios
+
+@app.get("/user/{id}")
+def obtener_usuario_id(id: int):    
+    for usuario in usuarios:
+        if usuario.id == id:
+            return usuario
+        else:
+            return {"message":f"Usuario con id = {id} no existe"}
+
+@app.delete("/users/{id}")
+def borrar_usuario(id: int):
+    for usuario in usuarios:
+        if usuario.id == id:
+            usuarios.remove(usuario)
+            return {"message":f"Usuario con id = {id} ha sido eliminado"}
+        else:
+            return {"message":f"Usuario con id = {id} no existe"}
+
+@app.post("/users")
+def crear_usuario(usuario: Usuario):
+    numero_usuarios = len(usuarios)
+    usuario.id = numero_usuarios + 1
+    usuarios.append(usuario)
+    return {"message": "Usuario Creado"}
