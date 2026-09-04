f6523a9856b3f49a1406822df645dd99138586b4
Darío S. Cabezas <dariosce99@gmail.com>
2026-09-04 11:04:29 -0500

    Refactor user handling in application.py

--- application.py
+++ application.py
@@ -3,41 +3,51 @@ from sqlmodel import SQLModel
 
 application = FastAPI()
 
+
 class Usuario(SQLModel):
     id: int
     nombre: str
     password: str
 
-usuarios = [Usuario(id=1, nombre="Juan", password="1234"),]
+
+usuarios = [
+    Usuario(id=1, nombre="Juan", password="1234"),
+]
+
 
 @application.get("/")
 def root():
     return {"message": "Hello world"}
 
+
 @application.get("/user")
 def obtener_usuarios():
     return usuarios
 
+
 @application.get("/user/{id}")
-def obtener_usuario_id(id: int):    
+def obtener_usuario_id(id: int):
     for usuario in usuarios:
         if usuario.id == id:
             return usuario
-        else:
-            return {"message":f"Usuario con id = {id} no existe"}
+
+    return {"message": f"Usuario con id = {id} no existe"}
+
 
 @application.delete("/users/{id}")
 def borrar_usuario(id: int):
     for usuario in usuarios:
         if usuario.id == id:
             usuarios.remove(usuario)
-            return {"message":f"Usuario con id = {id} ha sido eliminado"}
-        else:
-            return {"message":f"Usuario con id = {id} no existe"}
+            return {"message": f"Usuario con id = {id} ha sido eliminado"}
+
+    return {"message": f"Usuario con id = {id} no existe"}
+
 
 @application.post("/users")
 def crear_usuario(usuario: Usuario):
     numero_usuarios = len(usuarios)
     usuario.id = numero_usuarios + 1
     usuarios.append(usuario)
+
     return {"message": "Usuario Creado"}
